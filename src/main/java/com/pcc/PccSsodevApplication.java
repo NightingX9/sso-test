3d936d0022b3310a425716de32ad70aced3a55b5
Nighttt <Night7706@outlook.co.th>
2024-02-27 16:14:56 +0700

    first commit

---
+++ src/main/java/com/pcc/PccSsodevApplication.java
@@ -0,0 +1,13 @@
+package com.pcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PccSsodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PccSsodevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcc/PccSsodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PccSsodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
